8d8f60371e97c5112ee0f7faa3177156ac3acfb2
fedemza <fedevoloschin@gmail.com>
2025-04-24 14:31:38 -0300

    first action

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log("Hola, mundo!");
+require('dotenv').config();
+
+// saco el nombre de la variable de entorno
+nombre = process.env.NOMBRE || "sin nombre";
+console.log(`Hola ${nombre}, bien hecho!`);
